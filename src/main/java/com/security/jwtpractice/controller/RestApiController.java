6ea9ae2f54f540bc9402bcf84fa02e478d6b8e60
JamesWithCode <minseok.jeong.james@gmail.com>
2021-11-16 21:36:38 +0900

    mustache 뷰 리졸버 등록, 커스텀 설정

--- src/main/java/com/security/jwtpractice/controller/RestApiController.java
+++ src/main/java/com/security/jwtpractice/controller/RestApiController.java
@@ -6,12 +6,12 @@ import com.security.jwtpractice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //@CrossOrigin -> 공부하기
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class RestApiController {
 
