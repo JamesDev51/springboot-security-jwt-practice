653e02aff2290691befb79f302433ee2b7421676
JamesWithCode <minseok.jeong.james@gmail.com>
2021-11-19 00:56:50 +0900

    jwt-project-v2  initial commit

---
+++ jwt-project-v2/src/test/java/com/jamesdev/security/jwtv2/JwtV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesdev.security.jwtv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
