087e6d0a74f0ad0c4c55de5987f2ceb2a1e15efe
JamesWithCode <minseok.jeong.james@gmail.com>
2021-11-18 23:54:44 +0900

    jwt 리팩토링  SecurityConfig -1

--- src/main/java/com/security/jwtpractice/config/SecurityConfig.java
+++ src/main/java/com/security/jwtpractice/config/SecurityConfig.java
@@ -8,10 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
@@ -28,20 +31,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class); //시큐리티 필터 체인에 걸어주는 법
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //stateless -> 세션을 사용하지 않겠다.
-                .and()
-                .addFilter(corsFilter) //@CrossOrigin(인증 x ) , 시큐리티 필터에 등록(인증 o)
-                .formLogin().disable() //form 로그인 사용 안함
-                .logout()
-                .logoutUrl("/logout")
-                .deleteCookies(JwtProperties.HEADER_STRING)
-                .logoutSuccessUrl("/home")
-                .invalidateHttpSession(true)
-                .and()
-                .httpBasic().disable() //id,pw를 사용한 기본 인증방식을 사용하지 않고 토큰을 사용한 bearer 방식 사용
-                .addFilter(new JwtAuthenticationFilter(authenticationManager())) //authenticationManager를 param으로 줘야함
-                .addFilter(new JwtAuthorizationFilter(authenticationManager(),userRepository)) //authenticationManager를 param으로 줘야함
+        //2
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //3
+        http
+                .addFilter(corsFilter);
+
+        //4
+        http
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .authenticated()
@@ -50,5 +52,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/admin/**")
                 .access(" hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll();
+
+        //5
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .deleteCookies(JwtProperties.HEADER_STRING)
+                .logoutSuccessUrl("/home")
+                .invalidateHttpSession(true);
+
+        //6
+//        http
+//                .exceptionHandling()
+//                .authenticationEntryPoint();
+
+        //7
+        http
+                .addFilter(new JwtAuthenticationFilter(authenticationManager())) //authenticationManager를 param으로 줘야함
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),userRepository)); //authenticationManager를 param으로 줘야함
+    }
+
+
+    //8
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring();
+        web.httpFirewall(defaultHttpFirewall());
+    }
+    public HttpFirewall defaultHttpFirewall(){
+        return new DefaultHttpFirewall();
     }
 }
